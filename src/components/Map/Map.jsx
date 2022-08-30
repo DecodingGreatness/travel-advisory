2c822d8c5850121f620d5034d9a36885a38460ed
Edward Ho <pythoned77@gmail.com>
2022-08-30 11:06:36 +0800

    cleared unstaged files

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import ReactMapGL from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const Map = () => {
-  return <h1>Map</h1>;
+  return (
+    <ReactMapGL
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14,
+      }}
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+      style={{ width: 600, height: 400 }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
+  );
 };
 
 export default Map;
